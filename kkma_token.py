757067b6cf7e11d56a7f7b6d8af9616c69c1056d
eileenjang <eileen.jang2139@gmail.com>
2021-06-20 17:59:21 +0900

    Initial commit

---
+++ kkma_token.py
@@ -0,0 +1,45 @@
+import re
+import pandas as pd
+
+from konlpy.tag import Kkma
+
+def get_noun(msg_txt):
+    kkma=Kkma()
+    nouns=list()
+    pattern=re.compile("[ㄱ-ㅎㅏ-ㅣ]+")
+    msg_txt=re.sub(pattern, "", msg_txt).strip()
+
+    if len(msg_txt)>0:
+        pos=kkma.pos(msg_txt)
+        for keyword, type in pos:
+            if type == "NNG" or type == "NNP":
+                nouns.append(keyword)
+        print(msg_txt, "->", nouns)
+
+    return nouns
+
+def get_all_token(msg_txt):
+    kkma = Kkma()
+    nouns = list()
+    pattern = re.compile("[ㄱ-ㅎㅏ-ㅣ]+")
+    msg_txt = re.sub(pattern, "", msg_txt).strip()
+
+    if len(msg_txt) > 0:
+        pos = kkma.pos(msg_txt)
+        for keyword, type in pos:
+            nouns.append(keyword)
+        print(msg_txt, "->", nouns)
+
+    return nouns
+
+
+if __name__ == '__main__':
+    raw_data = pd.read_csv("./result/kko_regex.csv")
+    print(raw_data.head())
+    raw_data = raw_data.dropna()
+
+    raw_data["token"] = raw_data["contents"].apply(lambda x: get_noun(x))
+    raw_data.to_csv("./result/noun_token.csv", index=False)
+
+    raw_data["token"] = raw_data["contents"].apply(lambda x: get_all_token(x))
+    raw_data.to_csv("./result/all_token.csv", index=False)
---
+++ mwordcloud.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import ast
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from wordcloud import WordCloud
+
+
+def get_except_keyword(filename):
+    keyword_list = list()
+    with open(filename, encoding='utf-8') as f:
+        for keyword in f.readlines():
+            keyword_list.append(keyword.strip())
+    print(keyword_list)
+    return keyword_list
+
+
+def draw_wordcloud(kkma_result):
+    # List로 되어있는 열을 Row 단위로 분리
+    tokens = pd.DataFrame(kkma_result["token"].apply(lambda x: ast.literal_eval(x)).tolist())
+
+    tokens["Date"] = kkma_result["Date"]
+    tokens["Speaker"] = kkma_result["Speaker"]
+    tokens["timetype"] = kkma_result["timetype"]
+    tokens["time"] = kkma_result["time"]
+    tokens["contents"] = kkma_result["contents"]
+
+    tokens = tokens.set_index(["Date", "Speaker", "timetype", "time", "contents"])
+    tokens = tokens.T.unstack().dropna().reset_index()
+
+    tokens.columns = ["Date", "Person", "time_type", "time", "sntc", "index", "token"]
+    print(tokens.head())
+
+    # 빈도수 집계
+    summary = tokens.groupby(["token"])["index"].count().reset_index()
+    summary = summary.sort_values(["index"], ascending=[False]).reset_index(drop=True)
+
+    # 특정 단어 필터링
+    except_keyword = get_except_keyword("./raw_data/except_word.txt")
+    summary = summary[summary["token"].apply(lambda x: x not in except_keyword)]
+    summary = summary[summary["token"].apply(lambda x: len(x) > 1)]
+
+    # 이미지 Mask 생성
+    denne_mask = np.array(Image.open("./font/denne.png"))
+
+    # 워드클라우드 생성
+    wc = WordCloud(font_path='./font/NanumBrush.ttf', background_color='white', mask=denne_mask, width=800, height=600).generate(" ".join(summary["token"]))
+    plt.imshow(wc)
+    plt.axis("off")
+    plt.show()
+
+
+if __name__ == '__main__':
+    kkma_result = pd.read_csv("./result/noun_token.csv")
+    draw_wordcloud(kkma_result)
